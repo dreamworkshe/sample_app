6a2a07a0774866eabf26eaf8873cf877676f76d3
Yao-Cheng Li <dreamworkshe@gmail.com>
2012-07-08 22:19:45 +0800

    Finish the signup form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+	# Returns the Gravatar for the given user.
+	def gravatar_for(user, option = { size: 50 })
+		gravatar_id = Digest::MD5::hexdigest(user.email)
+		size = option[:size]
+		gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name "Cliff Lee"
+		email "clifflee@gmail.com"
+		password "lee321"
+		password_confirmation "lee321"
+	end
+end
